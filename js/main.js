81c39e1c3361d3fcb31a9804a25824e4ebddee0f
imqngali <55797666+imqngali@users.noreply.github.com>
2023-10-16 15:44:21 +0600

    161023

--- js/main.js
+++ js/main.js
@@ -166,7 +166,7 @@ jQuery(document).ready(function($) {
 	    loop: true,
 			stagePadding: 0,
 	    margin: 0,
-	    smartSpeed: 1500,
+	    smartSpeed: 3000,
 	    autoplay: false,
 	    pauseOnHover: false,
 	    dots: true,
@@ -271,6 +271,23 @@ jQuery(document).ready(function($) {
 	}
 	counter();
 
+	
 
+});
 
+$(document).ready(function () {
+     
+    $(".modal-link").on("click", function() {
+        $('.modal-overlay[data-modal-id="'+$(this).data('modal-id')+'"]').addClass("modal-overlay_visible");
+    });
+ 
+    $(".modal__close").on("click", function() {
+        $(".modal-overlay").removeClass("modal-overlay_visible");
+    });
+         
+    $(document).on("click", function(e) {
+        if(!$(e.target).closest(".modal").length && !$(e.target).closest(".modal-link").length) {
+            $(".modal-overlay").removeClass("modal-overlay_visible");
+        }
+    });
 });
