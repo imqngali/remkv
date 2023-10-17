2fec11bc960bf378c1e470a78abe1b4a3526b558
imqngali <55797666+imqngali@users.noreply.github.com>
2023-10-17 14:34:01 +0600

    ch171023r

--- js/main.js
+++ js/main.js
@@ -290,4 +290,24 @@ $(document).ready(function () {
             $(".modal-overlay").removeClass("modal-overlay_visible");
         }
     });
+
+	var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 });
